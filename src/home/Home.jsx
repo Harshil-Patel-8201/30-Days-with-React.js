7e6644094d550fb7ef6a669f28e107209bd8d82b
Harshil-Patel-8201 <harshilpatel33h@gmail.com>
2023-05-09 14:20:39 +0530

    commit

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import { HiOutlineMenuAlt4 } from "react-icons/hi";
-import { FaYoutube, FaGoogle, FaTwitter, FaPlay } from "react-icons/fa";
-import PlayerImage from "../assets/player.png";
 
 const Home = () => {
   return <div>heelo</div>;
