6d824c9161111150c8c58e798e6a16ae173c2c7b
Harshil Patel <harshilpatel33h@gmail.com>
2023-05-19 08:58:53 +0530

    commit

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,121 +1,81 @@
 import React from "react";
-import Logo from "../assets/logo.png"
-import ManPhoto from "../assets/man.png"
-import { FiArrowUpRight } from "react-icons/fi"
-import Jacket from '../assets/white_jacket.png'
-import Shoes from "../assets/shoes.png"
-import Cap from '../assets/cap.png'
+import Logo from "../assets/logo.png";
+import AppleLogo from "../assets/apple.png";
+import { IoMdMail } from "react-icons/io";
+import Circle from "../assets/blue_circle.png"
+import HandPhone from "../assets/mobile.png"
+import Icon1 from  "../assets/icon1.png"
+import Icon2 from  "../assets/icon2.png"
+import Icon3 from  "../assets/icon3.png"
 
 const Home = () => {
   return (
-    <div className="w-screen h-[820px] bg-white px-[100px] py-[10px] flex flex-col">
+    <div className="relative w-screen h-auto bg-colorWhite flex flex-col px-[100px]">
       {/* Header */}
-      <div className="flex flex-row justify-between items-center">
-        <div className="h-[60px] w-[100px] bg-colorBlack flex justify-center items-center rounded-xl">
-          <img src={Logo} className="w-[40px]" alt="" />
+      <div className="w-full flex flex-row py-5 items-center justify-between">
+        <img src={Logo} className="w-[120px]" alt="" />
+        <div className="flex flex-row gap-7">
+          <p className="text-sm text-black font-semibold">Home</p>
+          <p className="text-sm text-gray-400 font-semibold">About</p>
+          <p className="text-sm text-gray-400 font-semibold">Price</p>
+          <p className="text-sm text-gray-400 font-semibold">Download</p>
         </div>
-        <div className="flex flex-row gap-10">
-          <p className="text-sm text-black">Home</p>
-          <p className="text-sm text-black/40 font-thin">Trends</p>
-          <p className="text-sm text-black/40 font-thin">Store</p>
-          <p className="text-sm text-black/40 font-thin">Coopration</p>
-        </div>
-        <p className="text-white bg-colorBlack px-5 py-2 text-sm font-thin rounded-lg">
-          Contact Us
-        </p>
+        <img src={AppleLogo} className="w-[150px]" alt="" />
       </div>
 
-      {/* man picture row */}
-      <div className="relative w-full h-full">
-        <div className="absolute right-5 top-5">
-          <div className="w-[400px] h-[400px] rounded-full bg-gradient-to-r from-transparent via-white to-gray-300/50 flex justify-center items-center">
-            <div className="absolute w-[300px] h-[300px] rounded-full flex bg-gradient-to-r from-transparent to-white justify-center items-center"></div>
-          </div>
-        </div>
-
-        {/* Title text */}
-        <div className="absolute flex flex-col top-20 text-left gap-10">
-          <p className="text-black text-7xl font-semibold w-[300px] text-left tracking-wider">
-            Men's Autumn Collection
+      {/* Main Area */}
+      <div className="flex flex-row">
+        {/* Text */}
+        <div className="w-[380px] flex flex-col text-left mt-20">
+          <p className=" text-left text-5xl font-bold text-balck tracking-wider">
+            Mobile Banking and Invoicing
           </p>
-          <p className="text-xs text-white bg-colorBlue rounded-lg px-7 py-3 mr-auto shadow-xl shadow-colorBlue/30">
-            Open Catelog
+          <p className="text-black text-lg font-light tracking-wide py-10">
+            Control expenses, manage payments and cards on the go.
           </p>
+          <div className="w-full bg-white rounded-lg h-[60px] flex flex-row justify-between px-1 py-1 items-center">
+            <div className="flex flex-row items-center gap-2 ml-4">
+              <IoMdMail className="text-gray-300 text-xl" />
+              <p className="text-black text-sm tracking-wider">
+                mehmet@gmail.com
+              </p>
+            </div>
+            <p className="text-white bg-colorBlue px-3 py-3 rounded-lg mr-1">
+              Try it now
+            </p>
+          </div>
+          <div className="w-[250px] my-20 flex flex-row rounded-xl bg-colorBlue text-right justify-between shadow-lg shadow-gray-400">
+            <img
+              src={Circle}
+              className="w-[100px] h-[100px] -mt-8 -ml-2"
+              alt=""
+            />
+            <p className="w-[150px] text-white font-semibold text-base text-left my-7">
+              More than 5000 happy customers
+            </p>
+          </div>
         </div>
 
-        {/* Man Photo */}
-        <div className="relative text-right">
-          <img
-            src={ManPhoto}
-            className="w-[550px] absolute -top-[200px] -right-10"
-            alt=""
-          />
-          <div className="absolute w-[230px] h-[150px] bg-black/70 rounded-2xl right-[200px] top-[270px] px-5 py-3 flex flex-col text-left justify-between">
-            <p className="text-white text-lg font-normal">
-              All Jackets are discounted
-            </p>
-            <div className="flex flex-row items-center gap-3 justify-between">
-              <p className="text-6xl font-bold text-colorYello">35%</p>
-              <div className="px-3 py-3 rounded-lg border border-white flex justify-center items-center">
-                <FiArrowUpRight className="text-2xl text-white" />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Bottom Bar */}
-        <div className="absolute z-30 w-full h-[250px] rounded-3xl top-[470px] bg-gray-300/80 flex flex-row px-10 justify-between items-center">
-          <div className="flex flex-col text-left justify-between">
-            <p className="text-black tracking-wider font-semibold text-4xl">
-              Popular
-            </p>
-            <p className="text-black tracking-wider font-semibold text-4xl">
-              Products
-            </p>
-            <div className="w-full h-[1px] bg-colorBlack/70 my-5"></div>
-            <p className="text-colorBlack/70 text-2xl">Autum-Winter 2022</p>
-          </div>
-
-          {/* jacket */}
-          <div className="relative w-[230px] h-[180px] rounded-2xl bg-colorBlack flex justify-center items-center">
-            <img src={Jacket} className="w-[120px]" alt="" />
-            <div className="absolute flex flex-col justify-between h-[120px] mt-5 right-0">
-              <p className="text-white text-[18px] font-medium -rotate-90">
-                jackets
-              </p>
-              <div className="px-2 py-2 rounded-lg bg-white flex justify-center items-center mx-auto">
-                <FiArrowUpRight className="text-2xl text-black" />
-              </div>
-            </div>
-          </div>
-
-          {/* shoes */}
-          <div className="relative w-[230px] h-[180px] rounded-2xl bg-white flex justify-center items-center">
-            <img src={Shoes} className="w-[120px]" alt="" />
-            <div className="absolute flex flex-col justify-between h-[120px] mt-5 -right-2">
-              <p className="text-black text-[18px] font-medium -rotate-90">
-                sneakers
-              </p>
-              <div className="px-2 py-2 rounded-lg bg-colorBlack flex justify-center items-center mx-auto">
-                <FiArrowUpRight className="text-2xl text-white" />
-              </div>
-            </div>
-          </div>
-
-          {/* cap */}
-          <div className="relative w-[230px] h-[180px] rounded-2xl bg-colorBlue flex justify-center items-center">
-            <img src={Cap} className="w-[120px]" alt="" />
-            <div className="absolute flex flex-col justify-between h-[120px] mt-5 right-3">
-              <p className="text-white text-[18px] font-medium -rotate-90">
-                caps
-              </p>
-              <div className="px-2 py-2 rounded-lg bg-white flex justify-center items-center mx-auto">
-                <FiArrowUpRight className="text-2xl text-black" />
-              </div>
-            </div>
-          </div>
-        </div>
+        {/* Immage */}
+        <img src={HandPhone} className="ml-auto w-[450px]" alt="" />
       </div>
+
+      {/* Icons */}
+      <img
+        src={Icon1}
+        className="absolute w-[100px] right-[120px] top-[200px]"
+        alt=""
+      />
+      <img
+        src={Icon2}
+        className="absolute w-[80px] right-[300px] top-[600px]"
+        alt=""
+      />
+      <img
+        src={Icon3}
+        className="absolute w-[80px] right-[600px] top-[300px]"
+        alt=""
+      />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      
       colors: {
-        colorBlack: "#2c2c2c",
-        colorBlue: "#3965fc",
-        colorYello: "#fee3bc",
+        colorWhite: "#eaeff0",
+        colorBlue: "#4f5ae4",
       },
     },
   },
