78fba5adc3fa55db633221de510edfd8a816c8dc
Harshil Patel <harshilpatel33h@gmail.com>
2023-05-28 15:31:23 +0530

    commit

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,98 +1,96 @@
 import React from "react";
-import Logo from "../assets/logo.png";
-import Diamond from "../assets/diamond.png";
-import StyleBg from "../assets/styleBg.jpg";
-import Women from "../assets/women.png" 
-import {HiOutlineArrowUturnRight} from "react-icons/hi2"
+import { GiDiamonds } from "react-icons/gi";
+import { GoPrimitiveDot } from "react-icons/go";
+import Women from '../assets/women.png'
+import Sofa from '../assets/sofa.png'
+import Apple from "../assets/apple.png"
+import WoodSide from "../assets/woodside_logo.png"
 
 const Home = () => {
   return (
-    <div className="w-screen h-full bg-colorBackground flex flex-col px-[50px]">
+    <div className="relative w-screen h-full bg-colorBlack flex flex-col px-[30px]">
+      <GiDiamonds className="absolute z-10 top-[85px] right-[303px] text-colorTitle text-2xl" />
+      <GiDiamonds className="absolute z-10 top-[185px] left-[298px] text-colorTitle text-2xl" />
+      <GiDiamonds className="absolute z-10 top-[630px] left-[30px] text-colorTitle text-2xl" />
+
       {/* Header */}
-      <div className="w-full flex flex-row justify-between items-center border-[1.5px] rounded-full border-gray-600 my-[30px]">
-        <img src={Logo} className="w-[80px] ml-8" alt="" />
-        <div className="flex flex-row items-center gap-8 text-white/80 text-sm">
-          <p>EXHIBITION</p>
-          <p>ARTISTS</p>
-          <p>PRESS</p>
-          <p>SHOP</p>
+      <div className="w-full flex flex-row border-b-[1px] border-colorText/80 items-center">
+        <div className="w-[100%] flex flex-row justify-between items-center border-r-[1px] border-colorText/80 pr-[70px] py-[30px]">
+          <p className="text-colorTitle text-3xl -tracking-wide">WOODSIDE</p>
+          <div className="flex flex-row gap-8 text-colorText/70 text-sm">
+            <p>About us</p>
+            <p>Lifestyle</p>
+            <p>Boutiques & Staff</p>
+          </div>
         </div>
-        <div className="flex flex-row gap-5 items-center">
-          <p className="text-white/80 text-sm">CART</p>
-          <p className="h-14 w-14 text-black bg-colorYello p-3 rounded-full flex justify-center items-center text-2xl font-styleText">
-            0
+        <div className="w-[30%] flex items-center justify-center text-colorTitle px-[20px]">
+          <p>CONSULT ONLINE NOW</p>
+        </div>
+      </div>
+
+      {/* Home now */}
+      <div className="flex flex-row items-start mx-auto my-[10px] gap-10">
+        <p className="text-colorTitle text-[120px] font-extrabold">HOME</p>
+        <div className="flex flex-col">
+          <p className="text-sm text-colorText/70 text-left mt-10">
+            Online platform for mordern <p>home renovtion</p>
+            <div className="w-full h-[3px] bg-colorTitle mt-2"></div>
+          </p>
+        </div>
+        <p className="text-colorTitle text-[120px] font-extrabold">NOW</p>
+      </div>
+
+      {/* join as pro */}
+      <div className="flex flex-row my-[50px] justify-between mx-[50px] items-center">
+        <div className="flex flex-col items-center gap-[180px]">
+          <div className="flex flex-row bg-colorDarkGreen rounded-full gap-4 w-[140px] h-[100px] items-center justify-between -rotate-12">
+            <GoPrimitiveDot />
+            <p className="text-base font-semibold uppercase text-colorText ">
+              Join as <p>a pro</p>
+            </p>
+            <GoPrimitiveDot />
+          </div>
+          <p className="text-left text-colorTitle text-sm">
+            Home Design Ideas
+            <p className="text-colorTitle/40 text-xs">
+              Find interior design inspiration.
+            </p>
+          </p>
+        </div>
+        <div className="flex flex-col text-left ">
+          <p className="text-6xl text-colorTitle capitalize ">start</p>
+          <p className="text-6xl text-colorTitle capitalize ">Building</p>
+          <p className="text-6xl text-colorTitle capitalize ">Your</p>
+          <p className="text-6xl text-colorTitle capitalize ">Comfort</p>
+
+          <p className="text-colorText/80 underline text-sm mt-[50px]">
+            Get ideas
+          </p>
+        </div>
+        <div className="flex flex-col text-left gap-5 -rotate-12">
+          <div className="relative w-[150px] h-[150px] rounded-3xl bg-neutral-700">
+            <img
+              src={Women}
+              className="absolute bottom-0 w-[150px] h-[170px] object-cover"
+              alt=""
+            />
+          </div>
+          <p className="text=left text-sm text-colorText">
+            OUR <p>DESIGNERS</p>
           </p>
         </div>
       </div>
 
-      {/* Middel part */}
-      <div className="flex flex-row py-[50px] mx-auto">
-        {/* Text */}
-        <div className="h-[580px] w-[380px] bg-colorDark rounded-full flex flex-col  justify-center items-center gap-[60px] z-10 border-colorYello/50 border-2">
-          <img src={Diamond} className="w-7" alt="" />
-          <p className="text-white font-styleText text-7xl">
-            The<p>Gallery</p>
-          </p>
-          <p className="text-black bg-colorYello px-[30px] py-[20px] rounded-full text-sm font-bold">
-            DISCOVER
-          </p>
-        </div>
-
-        {/* bg style */}
-        <div className="relative h-[580px] w-[380px] -ml-10 ">
-          <img
-            src={StyleBg}
-            className="w-full h-full rounded-full border-white/50 border-2"
-            alt=""
-          />
-          <div className="absolute rotate-45 top-[250px] left-[150px] bg-colorDark h-20 w-20 flex justify-center items-center">
-            <p className="text-white -rotate-45 font-styleText text-5xl">&</p>
-          </div>
-        </div>
-
-        {/* Women */}
-        <div className="relative h-[580px] w-[380px] -ml-10 z-10 rounded-full border-b-[1.5px] border-r-[1.5px] border-gray-600">
-          <img
-            src={Women}
-            className="absolute bottom-0 right-0 rounded-br-[200px] rounded-l-[200px] w-[580px] h-[580px] object-cover"
-            alt=""
-          />
-          <p className="absolute top-0 right-5 text-white text-5xl text-left  font-styleText">
-            21st. <p>century</p>
-          </p>
-        </div>
-      </div>
-
-      {/* Bottom Bar */}
-      <div className="flex flex-row w-full justify-between py-[50px] px-[50px] items-center mb-[10px]">
-        <p className="text-white font-styleText text-left text-3xl">
-          Absolutely unique <p>bodv of work</p>
-        </p>
-        <p className="text-sm text-white/70 w-[400px] text-justify">
-          We creating connections between painting and sculpture in urban
-          settings, our artists adopt a thoroughly innovative approach in
-          artistic fusions in balanced combination of classical and
-          contemporary.
-        </p>
-        <div className="flex flex-row gap-5 items-center">
-          <div className="flex flex-row">
-            <img
-              src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
-              className="w-16 h-16 object-cover rounded-full"
-              alt=""
-            />
-            <img
-              src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
-              className="w-16 h-16 object-cover rounded-full -ml-5"
-              alt=""
-            />
-            <img
-              src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
-              className="w-16 h-16 object-cover rounded-full -ml-5"
-              alt=""
-            />
-          </div>
-          <HiOutlineArrowUturnRight className="text-2xl text-colorYello"/>
+      {/* Bottom pic bar */}
+      <div className="w-full flex flex-row mb-[50px] mt-[10px]">
+        <img
+          src={Sofa}
+          className="h-[200px] w-[850px] rounded-full object-cover bg-no-repeat bg-bottom"
+          alt=""
+        />
+        <div className="relative flex justify-center items-center w-[33%] h-[200px] rounded-full border-[1px] border-colorText/70">
+          <img src={Apple} className="w-[130px]" alt="" />
+          <img src={WoodSide} className="absolute w-[150px] -top-[72px] right-0" alt="" />
         </div>
       </div>
     </div>
